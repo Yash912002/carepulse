09a6dc5d372541f5e6206fb6eee5104e46114d6f
Yash Bhavsar <yashbhavsar02@gmail.com>
2024-07-18 11:49:04 +0530

    Add registerPatient and getUser function

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,7 +1,18 @@
-'use server'
+"use server";
 
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+import {
+	BUCKET_ID,
+	DATABASE_ID,
+	databases,
+	ENDPOINT,
+	PATIENT_COLLECTION_ID,
+	PROJECT_ID,
+	storage,
+	users,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+import { InputFile } from "node-appwrite/file";
 
 export const createUser = async (user: CreateUserParams) => {
 	try {
@@ -12,12 +23,54 @@ export const createUser = async (user: CreateUserParams) => {
 			undefined,
 			user.name
 		);
-    console.log(newUser)
+		console.log(newUser);
 	} catch (error: any) {
 		if (error && error?.code === 409) {
 			const documents = await users.list([Query.equal("email", [user.email])]);
 
 			return documents?.users[0];
 		}
+	}
+};
+
+export const getUser = async (userId: string) => {
+	try {
+		const user = await users.get(userId);
+		return parseStringify(user);
+	} catch (error: any) {
+		console.log(error);
+	}
+};
+
+export const registerPatient = async ({
+	identificationDocument,
+	...patient
+}: RegisterUserParams) => {
+	try {
+		let file;
+
+		if (identificationDocument) {
+			const inputFile = InputFile.fromBuffer(
+				identificationDocument?.get("blobFile") as Blob,
+				identificationDocument?.get("fileName") as string
+			);
+
+			file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+		}
+
+		const newPatient = await databases.createDocument(
+			DATABASE_ID!,
+			PATIENT_COLLECTION_ID!,
+			ID.unique(),
+			{
+				identificationDocumentId: file?.$id || null,
+				identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+				...patient
+			},
+		);
+
+		return parseStringify(newPatient);
+	} catch (error: any) {
+		console.log(error);
 	}
 };
