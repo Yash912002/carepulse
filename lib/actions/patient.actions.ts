f2fe58fa9ba5e79fc44f7b42dd3dc65af2f9bee1
Yash Bhavsar <yashbhavsar02@gmail.com>
2024-07-22 16:04:39 +0530

    Add getPatient function to fetch patient details by user ID

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -65,11 +65,24 @@ export const registerPatient = async ({
 			{
 				identificationDocumentId: file?.$id || null,
 				identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
-				...patient
-			},
+				...patient,
+			}
 		);
 
 		return parseStringify(newPatient);
+	} catch (error: any) {
+		console.log(error);
+	}
+};
+
+export const getPatient = async (userId: string) => {
+	try {
+		const patients = await databases.listDocuments(
+			DATABASE_ID!,
+			PATIENT_COLLECTION_ID!,
+			[Query.equal("userId", userId)]
+		);
+		return parseStringify(patients.documents[0]);
 	} catch (error: any) {
 		console.log(error);
 	}
